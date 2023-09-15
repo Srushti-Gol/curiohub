a39807f48444402996f4d0ac9b83ee92ad2f5527
Srushti <golsrushti1@gmail.com>
2023-09-15 07:35:55 +0530

    Bug fix

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,33 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "@mui/material";
 import axios from "axios";
 
 function SearchBar({ onSearch, onEmptySearch }) {
   const [searchQuery, setSearchQuery] = useState("");
 
-  const handleSearch = async () => {
-    if (searchQuery !== "") {
-      try {
-        const response = await axios.get(`/search?query=${searchQuery}`);
-        onSearch(response.data); // Pass search results to the parent component
-      } catch (error) {
-        console.error("Error:", error);
+  useEffect(() => {
+    const handleSearch = async () => {
+      if (searchQuery !== "") {
+        try {
+          const response = await axios.get(`/search?query=${searchQuery}`);
+          onSearch(response.data); // Pass search results to the parent component
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      } else {
+        onEmptySearch();
       }
-    } else {
-    //   alert("Type something");
-      onEmptySearch(); // Notify the parent that the search query is empty
-    }
-  };
+    };
+
+    const identifier = setTimeout(() => {
+      if (searchQuery === "") {
+        onEmptySearch();
+      } else {
+        handleSearch();
+      }
+    }, 500);
+
+    return () => {
+      clearTimeout(identifier);
+    };
+  }, [searchQuery, onSearch, onEmptySearch]);
 
   const handleChange = (e) => {
     setSearchQuery(e.target.value);
-
-    // Automatically trigger search when the input changes
-    if (e.target.value === "") {
-      onEmptySearch(); // Notify the parent that the search query is empty
-    } else {
-      handleSearch();
-    }
   };
 
   return (
